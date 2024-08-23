710c0b9905f30efa8617283e9ba5771ab1df23b9
AnyaVeselova <anna.correspondence@outlook.com>
2024-08-23 19:59:52 +0100

    creates add task form in new-task.component.css and new-task.component.html

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,18 +1,20 @@
 import { Component, Input, Output, EventEmitter, output } from '@angular/core';
 import { TaskComponent } from './task/task.component';
 import { dummyTasks } from '../dummy-tasks';
+import { NewTaskComponent } from './new-task/new-task.component';
 @Component({
   selector: 'app-tasks',
   standalone: true,
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
 })
 export class TasksComponent {
   @Input({ required: true }) userId!: string;
   @Input({ required: true }) name!: string;
   @Output() select = new EventEmitter<string>();
   tasks = dummyTasks;
+  isAddingTask = false;
 
   get selectedUserTask() {
     return this.tasks.filter((task) => task.userId === this.userId);
@@ -20,5 +22,9 @@ export class TasksComponent {
 
   onCompleteTask(id: string) {
     this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+
+  onAddingTask() {
+    this.isAddingTask = true;
   }
 }
