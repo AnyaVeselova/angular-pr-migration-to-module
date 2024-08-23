2f8a15ae4e259272c6f5f7356a8ab13245a7955a
AnyaVeselova <anna.correspondence@outlook.com>
2024-08-23 17:18:03 +0100

    outputs user specific tasks by creating selectedUserTask getter, filtering dummyTasks array and binding selecteUser id with the id in the tasks array

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, Output, EventEmitter, output } from '@angular/core';
 import { TaskComponent } from './task/task.component';
-
+import { dummyTasks } from '../dummy-tasks';
 @Component({
   selector: 'app-tasks',
   standalone: true,
@@ -9,5 +9,11 @@ import { TaskComponent } from './task/task.component';
   imports: [TaskComponent],
 })
 export class TasksComponent {
-  @Input() name?: string;
+  @Input({ required: true }) userId!: string;
+  @Input({ required: true }) name!: string;
+  tasks = dummyTasks;
+
+  get selectedUserTask() {
+    return this.tasks.filter((task) => task.userId === this.userId);
+  }
 }
